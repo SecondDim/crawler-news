c71cc4f31b86afe887343758528c6c6fd05939f7
SecondDim <8318845+SecondDim@users.noreply.github.com>
2020-02-02 08:35:31 +0800

    update

--- run_spiders.sh
+++ run_spiders.sh
@@ -3,10 +3,13 @@ do
     spiders=$(scrapy list)
     for spider in $spiders
     do
-        echo $spider
+        if [ "${spider}" == "localhost" ]; then
+            continue
+        fi
+        echo "[+] ${spider}"
         scrapy crawl $spider
     done
-    echo '[*] Done.wait 5 min'
-    sleep 5m
+    echo '[*] Done.wait 5 second'
+    sleep 5
 done
 
