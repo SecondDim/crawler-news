07fe10b1d0a4cecd89dcf4b296f65a18506088c9
SecondDim <8318845+SecondDim@users.noreply.github.com>
2020-06-09 11:03:23 +0800

    fix cicd

--- .circleci/circleci.sh
+++ .circleci/circleci.sh
@@ -5,6 +5,6 @@ set -xe
 cd /home/ubuntu/crawler-news
 git checkout . && git checkout $1 && git pull
 cp .circleci/docker-compose.yml docker-compose.yml
-cp /home/ubuntu/config/settings.py crawler_news/
+cp /home/ubuntu/config/settings.py docker/
 sudo docker stop crawler_news && sudo docker rm crawler_news
 sudo docker-compose up -d --build
